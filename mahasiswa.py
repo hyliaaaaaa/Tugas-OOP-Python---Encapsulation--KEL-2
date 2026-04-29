04649c435d104642ca3d3ab002c5866c5dc8f666
unknown <hanyemalia10@gmail.com>
2026-04-29 10:53:44 +0700

    Nambahin kode awal ya (by hany, lanjutin guys)

---
+++ mahasiswa.py
@@ -0,0 +1,29 @@
+class Mahasiswa: 
+    def __init__(self, nama, nim):
+        self.__nama = nama
+        self.__nim = nim
+        self.__nilai_tugas = 0
+        self.__nilai_uts = 0
+        self.__nilai_uas = 0
+        self.__nilai_akhir = 0
+
+    #ini bagian untuk memvalidasi nilainya ya
+    def __validasi(self, nilai):
+        return 0 <= nilai <= 100
+
+    #Menghitung nilai akhirnya
+    def menghitung_nilai_akhir(self):
+        self.__nilai_akhir = (
+            self.__nilai_tugas * 0.3 +   #nilai tugas nya diambil 30 % (kami menggunakan desimal (float))
+            self.__nilai_uts * 0.3 +     #nilai uts nya diambil 30 %
+            self.__nilai_uas * 0.4       #nilai uas nya diambil 30 %
+        )
+        
+    def set_nilai(self, tugas, uts, uas):
+        if self.__validasi(tugas) and self.__validasi(uts) and self.__validasi(uas):
+            self.__nilai_tugas = tugas
+            self.__nilai_uts = uts
+            self.__nilai_uas = uas
+            self.menghitung_nilai_akhir()
+        else:
+            print("Masukkan nilai dengan angka anatar 0 - 100")
